6d76322b29d2474755a2df5dcc15def1a9ae4851
alejo-devs <alejandro.guerrau@udea.edu.co>
2025-05-30 12:31:05 -0500

    primer commit

---
+++ src/app/pruebas/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '@/components/atoms/buttons/page'
+import Icon from '@/components/atoms/icon/page'
+
+const Page = () => {
+  return (
+    <div>
+    <Button />
+    <Icon icon={'material-symbols:10k'} />
+    </div>
+  )
+}
+
+export default Page
---
+++ src/components/atoms/buttons/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Butones</div>
+  )
+}
+
+export default Page
